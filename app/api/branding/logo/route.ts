d6da7d3a6cd07d95d8a8b6b82e244331d00bc42d
sewlesew <sewlesewbiazen65@gmail.com>
2026-05-10 19:23:43 +0300

    commit

--- app/api/branding/logo/route.ts
+++ app/api/branding/logo/route.ts
@@ -1,14 +1,14 @@
 import fs from "fs/promises";
 import { NextResponse } from "next/server";
 
-const ATTACHED_LOGO_PATH ="public\images\logo\photo_2026-05-10_19-01-38.jpg"
+const ATTACHED_LOGO_PATH ="public/images/logo/photo_2026-05-10_19-01-38.jpg"
 
 export async function GET() {
   try {
     const image = await fs.readFile(ATTACHED_LOGO_PATH);
     return new NextResponse(new Uint8Array(image), {
       headers: {
-        "Content-Type": "image/png",
+        "Content-Type": "image/jpg",
         "Cache-Control": "public, max-age=31536000, immutable",
       },
     });
